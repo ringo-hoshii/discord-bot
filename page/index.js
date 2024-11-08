a6031e5d9c0305a425594b44805bedbfbbd3525f
wantapples <ringo-hoshii@users.noreply.github.com>
2024-11-08 15:34:31 +0300

    started making a local node.js server

---
+++ page/index.js
@@ -0,0 +1,31 @@
+const http = require("http") 
+const fs = require("fs")
+const util = require("util")
+
+let port = 80
+
+let filename = "incomingrequests.txt"
+let incomingreqstream = fs.createWriteStream(filename, { flags: "a" })
+
+let locallog = []
+locallog.last = () => locallog.length - 1
+
+let debug
+
+function log(message) {
+    console.log(message)
+    let messagestring = util.inspect(message)
+    debug = messagestring
+    console.log("fs is writing " + typeof messagestring)
+    incomingreqstream.write(messagestring)
+    locallog.push(message)
+}
+
+// req - incoming request
+// res - object for answering back to the request issuer
+function requesthandler(req, res) {
+    console.log(req.headers)
+    incomingreqstream.write(util.inspect(req))
+}
+
+http.createServer(requesthandler).listen(port)
